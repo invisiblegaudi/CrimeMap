5b4eb3009fcb1ad0e98b0a5a94840c778a954415
John Kavanagh <john@invisiblearchitects.com>
2017-07-05 14:48:59 +0100

    renamed to crimemap to prevent nameclash

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import GoogleMap from './components/GoogleMap/GoogleMap.js'
+import CrimeMap from './components/CrimeMap/CrimeMap.js';
 
 class App extends Component {
     
@@ -8,7 +8,7 @@ class App extends Component {
 	return (
 	    <div className="App">
             <h2>Crime Map</h2>
-	    <GoogleMap />
+	    <CrimeMap />
 	    </div>
 	);
     }
