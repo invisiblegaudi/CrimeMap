fa704ac37b8123b7d73d56de7b41a88993b878b9
John Kavanagh <john@invisiblearchitects.com>
2017-07-05 14:32:16 +0100

    maps component rendering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import GoogleMap from './components/GoogleMap/GoogleMap.js'
 
 class App extends Component {
+    
     render() {
 	return (
 	    <div className="App">
             <h2>Crime Map</h2>
+	    <GoogleMap />
 	    </div>
 	);
     }
